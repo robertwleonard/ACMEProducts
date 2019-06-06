1ad2be048581409cf992aff3d8a33806967f7b74
Robert W. Leonard <robert.leonard@birdvilleschools.net>
2019-06-06 08:51:04 -0500

    Set up observables for products

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 150;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     
     _listFilter: string;
     get listFilter(): string {
@@ -45,7 +46,12 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe (
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
     }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,33 +1,33 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    private productUrl = 'api/products/products.json';
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2016",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "https://cdn.pixabay.com/photo/2019/06/04/19/54/norway-4252178_960_720.jpg"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2016",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "https://cdn.pixabay.com/photo/2012/11/06/15/40/tree-64310_960_720.jpg"
-            }
-        ];
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+
+        console.error(errorMessage);
+        return throwError(errorMessage);
     }
 }
